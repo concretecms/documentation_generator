66139d7ac7c50292e10c8e2ccd4f229557f109fa
Korvin Szanto <korvinszanto@gmail.com>
2015-08-22 15:22:05 -0700

    Update to version 1.0-beta1

--- config/generator.php
+++ config/generator.php
@@ -7,6 +7,7 @@ return array(
         "block_type" => "\\Concrete\\DocumentationGenerator\\Generator\\BlockType\\BlockTypeListGenerator",
         "config_group" => "\\Concrete\\DocumentationGenerator\\Generator\\ConfigGroup\\ConfigGroupListGenerator",
         "vendor_javascript" => "\\Concrete\\DocumentationGenerator\\Generator\\Asset\\VendorJavascriptAssetListGenerator",
-        "vendor_css" => "\\Concrete\\DocumentationGenerator\\Generator\\Asset\\VendorCssAssetListGenerator"
+        "vendor_css" => "\\Concrete\\DocumentationGenerator\\Generator\\Asset\\VendorCssAssetListGenerator",
+        "vendor_libraries" => "\\Concrete\\DocumentationGenerator\\Generator\\Vendor\\VendorLibraryListGenerator"
     )
 );
